4450d7d06ea6515ff22648757f7c6b7abcd6aaf9
Liang C <sl20130313@gmail.com>
2021-11-10 22:48:58 +0800

    feat : implement getState

--- redux.js
+++ redux.js
@@ -1,12 +1,23 @@
 window.Redux = {
   createStore(reducer, preloadedState) {
     let currentState = preloadedState;
+    let isDispatching = false;
 
-    function getState() {}
+    function getState() {
+      if (isDispatching) {
+        throw new Error(
+          "You may not call store.getState() while the reducer is executing. " +
+            "The reducer has already received the state as an argument. " +
+            "Get the state from the top reducer instead of reading it from the store."
+        );
+      }
 
-    function dispatch(action) {}
+      return currentState;
+    }
 
-    function subscribe(listener) {}
+    function dispatch() {}
+
+    function subscribe() {}
 
     const store = {
       getState,
