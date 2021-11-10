314638be0633e92782780b4e226f7a06ab165def
Liang C <sl20130313@gmail.com>
2021-11-10 23:04:29 +0800

    feat : implement subscribe

--- redux.js
+++ redux.js
@@ -2,6 +2,7 @@ window.Redux = {
   createStore(reducer, preloadedState) {
     let currentState = preloadedState;
     let currentReducer = reducer;
+    let listeners = [];
     let isDispatching = false;
 
     function getState() {
@@ -29,9 +30,43 @@ window.Redux = {
       } finally {
         isDispatching = false;
       }
+
+      for (let i = 0; i < listeners.length; i++) {
+        const listener = listeners[i];
+        listener();
+      }
     }
 
-    function subscribe() {}
+    function subscribe(listener) {
+      if (isDispatching) {
+        throw new Error(
+          "You may not call store.subscribe() while the reducer is executing. " +
+            "If you would like to be notified after the store has been updated, " +
+            "subscribe from a component and invoke store.getState() in the callback to access the latest state."
+        );
+      }
+
+      let isSubscribed = true;
+
+      listeners.push(listener);
+
+      return function unsubscribe(listener) {
+        if (!isSubscribed) {
+          return;
+        }
+
+        if (isDispatching) {
+          throw new Error(
+            "You may not unsubscribe from a store listener while the reducer is executing. "
+          );
+        }
+
+        const index = listeners.indexOf(listener);
+        listeners.splice(index, 1);
+
+        isSubscribed = false;
+      };
+    }
 
     const store = {
       getState,
